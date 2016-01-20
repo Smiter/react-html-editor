6f7c82b4a7aa84ac021962ca6cc75de76157f846
Serhii Mykhailenko <smiter4@gmail.com>
2016-01-20 01:01:27 -0500

    refactoring

--- client/reducers/blocks.jsx
+++ client/reducers/blocks.jsx
@@ -2,10 +2,6 @@
 import { handleActions } from 'redux-actions'
 
 const initialState = [
-// [{ id: 0}, { id: 1}],[{id: 2}, { id: 3}]
-// [{ id: 0}],[{id: 2}, { id: 3}]
-// [{ id: 0}, { id: 1}],[{id: 2}]
-// [{ id: 0}],[{id: 2}, { id: 3}, { id: 4}]
   [
  
     [{ id: 0 }] 
