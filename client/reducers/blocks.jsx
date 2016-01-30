9148982acb5fa2bb707eafa16f363c652dfcde49
Serhii Mykhailenko <smiter4@gmail.com>
2016-01-29 23:50:14 -0500

    updated reducer

--- client/reducers/blocks.jsx
+++ client/reducers/blocks.jsx
@@ -6,13 +6,13 @@ const layouts = [
   [
     [
       {
-        width: "75%",
+        width: "100%",
         col_blocks: [{ type: blocktypes.TEXT}]
       } 
     ],
     [
       {
-        width: "50%",
+        width: "100%",
         col_blocks: [{ type: blocktypes.IMAGE, src: ''}]
       } 
     ]
@@ -20,40 +20,82 @@ const layouts = [
 
   [
     [
-      [{ type: blocktypes.TEXT}]       
+      {
+        width: "100%",
+        col_blocks: [{ type: blocktypes.TEXT}]  
+      }     
     ],
     [
-      [{ type: blocktypes.IMAGE, src: '' }],
-      [{ type: blocktypes.TEXT }]
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.IMAGE, src: '' }]
+      },
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }
     ]
   ],
     [
     [
-      [{ type: blocktypes.TEXT}]       
+      {
+        width: "100%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }     
     ],
     [
-      [{ type: blocktypes.IMAGE, src: '' }],
-      [{ type: blocktypes.TEXT }],
-      [{ type: blocktypes.TEXT }]
+      {
+        width: "33.3333%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      },
+      {
+        width: "33.3333%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      },
+      {
+        width: "33.3333%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }
     ]
   ],
   [
     [
-      [{ type: blocktypes.IMAGE, src: '' }],
-      [{ type: blocktypes.TEXT }]
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      },
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }
     ],
     [
-      [{ type: blocktypes.TEXT}]       
+      {
+        width: "100%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }       
     ],
   ],
   [
     [
-      [{ type: blocktypes.IMAGE, src: '' }],
-      [{ type: blocktypes.TEXT }]
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      },
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }
     ],
     [
-      [{ type: blocktypes.IMAGE, src: '' }],
-      [{ type: blocktypes.TEXT }]
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      },
+      {
+        width: "50%",
+        col_blocks: [{ type: blocktypes.TEXT }]
+      }
     ],
   ],
 
@@ -70,18 +112,18 @@ export default handleActions({
   'add block' (state, action) {
     const {blockType, indexes: {row_idx, col_idx, block_idx}} = action.payload
     let a = Object.assign({}, state, {})
-    let b = a.blocks[row_idx][col_idx];
+    let b = a.blocks[row_idx][col_idx].col_blocks;
     b = [...b.slice(0, block_idx+1), { type: blockType }, ...b.slice(block_idx+1)]
-    a.blocks[row_idx][col_idx] = b
+    a.blocks[row_idx][col_idx].col_blocks = b
     return a
   },
 
   'delete block' (state, action) {
     const {row_idx, col_idx, block_idx} = action.payload.indexes
     let a = Object.assign({}, state, {})
-    let b = a.blocks[row_idx][col_idx];
+    let b = a.blocks[row_idx][col_idx].col_blocks;
     b = [...b.slice(0, block_idx), ...b.slice(block_idx+1)]
-    a.blocks[row_idx][col_idx] = b
+    a.blocks[row_idx][col_idx].col_blocks = b
     return a
   },
 
